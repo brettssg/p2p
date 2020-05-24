fbb45976f9a12ce6d46cb1df442c301ae9ee7f7c
brettssg <65871308+brettssg@users.noreply.github.com>
2020-05-24 21:52:55 +0100

    Add files via upload

---
+++ sample_pythonanywhere_wsgi.py
@@ -0,0 +1,13 @@
+  
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/brettssg/p2p'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# need to pass the flask app as "application" for WSGI to work
+# for a dash app, that is at app.server
+# see https://plot.ly/dash/deployment
+from appv24 import app
+application = app.server
